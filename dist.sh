0257f2c57e1ffa25d8787546394f99daebe8c775
Luis Parravicini <lparravi@gmail.com>
2013-06-01 16:09:33 -0300

    v1.1

--- dist.sh
+++ dist.sh
@@ -2,5 +2,6 @@
 
 zip=editorHuesos.zip
 
-./build.sh && cd docs && markdown && cd .. && zip $zip -X9 docs/* && zip $zip -jX9 target/editorHuesos-release-1.0-SNAPSHOT.jar
+[ -f "$zip" ] && rm "$zip"
+./build.sh && cd docs && markdown && cd .. && zip $zip -X9 docs/* && zip $zip -jX9 target/editorHuesos-release-*
 
