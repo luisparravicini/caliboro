b5116e2d63680e62198ed24e8c7424ae584874c3
Luis Parravicini <lparravi@gmail.com>
2013-06-13 09:08:59 -0300

    mensaje de error

--- src/main/java/ar/com/ktulu/editorHuesos/ui/images/ImageException.java
+++ src/main/java/ar/com/ktulu/editorHuesos/ui/images/ImageException.java
@@ -6,7 +6,7 @@ public class ImageException extends Exception {
 	private static final long serialVersionUID = 5315206931164996184L;
 
 	public ImageException(Exception e) {
-		super("Formato de imagen no reconocido", e);
+		super("Formato de imagen no reconocido. Solo se permiten png y jpg (en rgb)", e);
 	}
 
 }
