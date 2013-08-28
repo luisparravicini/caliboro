998c89a004051f128e25636806eddf11244d6690
Luis Parravicini <lparravi@gmail.com>
2013-08-28 18:17:43 -0300

    dnd basico

---
+++ src/main/java/ar/com/ktulu/caliboro/ui/dnd/BonesTransferHandler.java
@@ -0,0 +1,108 @@
+package ar.com.ktulu.caliboro.ui.dnd;
+
+import java.awt.datatransfer.Transferable;
+
+import javax.swing.JComponent;
+import javax.swing.JTree;
+import javax.swing.TransferHandler;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+
+import ar.com.ktulu.caliboro.ui.treeModel.BaseBoneTreeNode;
+import ar.com.ktulu.caliboro.ui.treeModel.BoneTreeNode;
+
+public class BonesTransferHandler extends TransferHandler {
+
+	private static final long serialVersionUID = -7783801799223806678L;
+	private BaseBoneTreeNode sourceNode;
+
+	@Override
+	public boolean canImport(TransferSupport support) {
+		if (!support.isDrop())
+			return false;
+
+		JTree tree = (JTree) support.getComponent();
+		TreePath srcPath = tree.getSelectionPath();
+		if (srcPath == null)
+			return false;
+
+		support.setShowDropLocation(true);
+
+		return true;
+	}
+
+	@Override
+	protected Transferable createTransferable(JComponent c) {
+		TreePath path = ((JTree) c).getSelectionPath();
+		if (path != null) {
+			DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) path
+					.getLastPathComponent();
+			if (BaseBoneTreeNode.class.isInstance(selectedNode)) {
+				this.sourceNode = (BaseBoneTreeNode) selectedNode;
+				return new TransferableItem((BaseBoneTreeNode) selectedNode);
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void exportDone(JComponent source, Transferable data, int action) {
+		this.sourceNode = null;
+	}
+
+	public int getSourceActions(JComponent c) {
+		return MOVE;
+	}
+
+	public boolean importData(TransferHandler.TransferSupport support) {
+		JTree tree = (JTree) support.getComponent();
+		JTree.DropLocation dropLocation = (JTree.DropLocation) support
+				.getDropLocation();
+
+		TreePath dropPath = dropLocation.getPath();
+		BaseBoneTreeNode dstNode = null;
+		if (dropPath != null)
+			dstNode = (BaseBoneTreeNode) dropPath.getLastPathComponent();
+
+		BaseBoneTreeNode source = this.sourceNode;
+		boolean sourceIsBone = (BoneTreeNode.class.isInstance(source));
+		DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+
+		if (sourceIsBone) {
+			DefaultMutableTreeNode parent = (DefaultMutableTreeNode) model
+					.getRoot();
+			if (dstNode == null) {
+				model.removeNodeFromParent(source);
+				model.insertNodeInto(source, parent,
+						model.getChildCount(parent));
+			} else {
+				int index = model.getIndexOfChild(parent, dstNode);
+				if (index >= 0) {
+					model.removeNodeFromParent(source);
+					model.insertNodeInto(source, parent, index);
+				}
+			}
+		} else if (dstNode != null) {
+			boolean dstIsBone = (BoneTreeNode.class.isInstance(dstNode));
+
+			if (dstIsBone) {
+				DefaultMutableTreeNode parent = dstNode;
+				model.removeNodeFromParent(source);
+				model.insertNodeInto(source, parent,
+						model.getChildCount(parent));
+			} else {
+				DefaultMutableTreeNode parent = (DefaultMutableTreeNode) dstNode.getParent();
+				int index = model.getIndexOfChild(parent, dstNode);
+				if (index >= 0) {
+					model.removeNodeFromParent(source);
+					model.insertNodeInto(source, parent, index);
+				}
+			}
+		}
+
+		return true;
+	}
+
+}
---
+++ src/main/java/ar/com/ktulu/caliboro/ui/dnd/TransferableItem.java
@@ -0,0 +1,42 @@
+package ar.com.ktulu.caliboro.ui.dnd;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+import ar.com.ktulu.caliboro.ui.treeModel.BaseBoneTreeNode;
+
+public class TransferableItem implements Transferable {
+
+	private BaseBoneTreeNode node;
+
+	public TransferableItem(BaseBoneTreeNode node) {
+		this.node = node;
+	}
+
+	@Override
+	public Object getTransferData(DataFlavor flavor)
+			throws UnsupportedFlavorException, IOException {
+		if (TransferableItem.getBaseBoneTreeNode().equals(flavor))
+			return node.getPath();
+		else
+			return null;
+	}
+
+	public static DataFlavor getBaseBoneTreeNode() {
+//		return new DataFlavor(BaseBoneTreeNode.class, "NodoTransferible");
+		return new DataFlavor(BaseBoneTreeNode.class, "TransferibleNode");
+	}
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return new DataFlavor[] { TransferableItem.getBaseBoneTreeNode(), };
+	}
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		return TransferableItem.getBaseBoneTreeNode().equals(flavor);
+	}
+
+}
