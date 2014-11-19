9f9f9eeabbd06c1ec0be8a23b3a3b62a13bfc8cc
Luis Parravicini <lparravi@gmail.com>
2014-11-19 15:39:43 -0300

    script para listar imagenes sin usar

---
+++ img_unused.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+#
+# Lista las imagenes de cierto path que no estan siendo referenciadas por Caliboro
+#
+
+dir = ARGV.shift
+if dir.nil?
+  puts "usage: #{$0} <path>"
+  exit 1
+end
+
+
+def used_files(path)
+  json = JSON.load(IO.read(File.join(path, 'bones.json')))
+
+  json['bones'].map { |x| x['images'] }.map do |img|
+    img.map { |a| a['imagePath'] }
+  end.flatten
+end
+
+
+Dir.glob(File.join(dir, '*')).each do |path|
+  next unless File.directory?(path)
+
+  puts "-"*60
+  puts path
+  used = used_files(path)
+  Dir.glob(File.join(path, '*')).each do |subpath|
+    name = File.basename(subpath)
+    next unless name =~ /\.jpg$/i
+
+    puts subpath unless used.include?(name)
+  end
+
+
+  used.each do |img_path|
+    raise "#{img_path} not found" unless File.exist?(File.join(path, img_path))
+  end
+
+end
+
